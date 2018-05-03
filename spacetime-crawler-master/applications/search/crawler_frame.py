f97aab614ec3301c1ac417b0e5afbbc9856f250b
Brandon Delehoy <bdelehoy@yahoo.com>
2018-05-02 17:18:36 -0700

    Added basic incorrect version of extract_next_links

--- spacetime-crawler-master/applications/search/crawler_frame.py
+++ spacetime-crawler-master/applications/search/crawler_frame.py
@@ -66,15 +66,21 @@ def extract_next_links(rawDataObj):
     Suggested library: lxml
     '''
     print("***********URL:           ", rawDataObj.url)             # self explanatory i hope
-    print("***********CONTENT:       ", rawDataObj.content)         # ALL the html source of the page.  parse this for links.
+    #print("***********CONTENT:       ", rawDataObj.content)         # ALL the html source of the page.  parse this for links.
     print("***********ERROR MSG:     ", rawDataObj.error_message)   # "not found", etc.
     print("***********HEADERS:       ", rawDataObj.headers)         # part of the handshake
     print("***********HTTP CODE:     ", rawDataObj.http_code)       # the 3 digit http code (like 404, etc.)
     print("***********IS REDIRECTED: ", rawDataObj.is_redirected)   # how to tell is this is a trap!
     print("***********FINAL URL:     ", rawDataObj.final_url)       # i think this only gets a value if this URL redirects you somewhere
-
-    # add all the URL -STRINGS- to outputLinks
     
+    # add all the URL -STRINGS- to outputLinks
+    links = html.iterlinks(rawDataObj.content)  # returns a list of ALL links, even to things like stylesheets and image assets
+    #links = html.find_rel_links(rawDataObj.content, 'href')
+    
+    for link in links:
+        outputLinks.append(link[2])
+
+    print "*****WOW*****", outputLinks
     return outputLinks
 
 def is_valid(url):
