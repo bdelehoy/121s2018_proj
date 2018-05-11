6c3bb717fe09b8b4b858f5682765b3073e23c36b
Brandon Delehoy <bdelehoy@yahoo.com>
2018-05-11 15:56:18 -0700

    framework.wav

--- assignment4_5/build_indices.py
+++ assignment4_5/build_indices.py
@@ -5,15 +5,38 @@
 # we're gonna have to have someone run this overnight so we can get all the tokens in the database
 # i (brandon) can do that on my desktop at home (ryzen 5 1600) at some point
 
+import json
 import pymongo
 from bs4 import BeautifulSoup
 
 MAX_ENTRIES = 10
+FOLDERMAX = 74
+FILEMAX = 499
+
+BK_PATH = './WEBPAGES_RAW/bookkeeping.json'
+with open(BK_PATH) as f:
+    PAGES = json.load(f)
+
+def get_ids():
+    result = []
+    counter = 0
+    for folder in range(FOLDERMAX+1):
+        for file in range(FILEMAX+1):
+            counter += 1
+            if counter <= MAX_ENTRIES:  # accounts for only collecting up to MAX_ENTRIES documents
+                one_id = str(folder) + '/' + str(file)
+                result.append(one_id)
+    return result
 
 def main():
     print "Starting to build indices:"
-    for i in range(MAX_ENTRIES):
-        print "whee", i
+    all_ids = get_ids()
+    for docid in all_ids:
+        try:
+            print "Tokenizing document {}: {}\n".format(docid, PAGES[docid])
+            
+        except KeyError:
+            print "File does not exist at", docid
 
 if __name__ == "__main__":
     main()
