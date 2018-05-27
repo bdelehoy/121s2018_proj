a0b3fda4a697d48b623a52b3746893160ba78dbb
Mayan Shoshani <mshoshan@uci.edu>
2018-05-26 19:22:12 -0700

    now is an actual server

--- assignment4_5/webpage/server.js
+++ assignment4_5/webpage/server.js
@@ -1,29 +1,39 @@
 console.log("testing")
+const express = require('express');
+const app = express();
+app.listen(3000, function() {console.log('listening on 3000')})
+
+
+app.get('/', (req, res) => {
+    var spawn = require("child_process").spawn;
+    var process = spawn('python', ["C:/Users/Shoshani/Documents/card-algorithms/Space-Jam/assignment4_5/query_database.py","four"]);
 
 
 
+    process.stdout.on('data', function (chunk) {
+        console.log("data returned")
+        var textChunk = chunk.toString();// buffer to string
 
-var spawn = require("child_process").spawn;
-var process = spawn('python', ["C:/Users/Shoshani/Documents/card-algorithms/Space-Jam/assignment4_5/query_database.py","four"]);
+        re.send(textChunk);
+    });
+})
 
 
 
-process.stdout.on('data', function (chunk) {
-    console.log("data returned")
-    var textChunk = chunk.toString();// buffer to string
-
-    console.log(textChunk);
-});
 /*
-var spawn = require("child_process").spawn;
-var process = spawn('python', ["C:/Users/Shoshani/Documents/card-algorithms/Space-Jam/assignment4_5/webpage/compute_input.py", "mayan"]);
+app.get('/', (req, res) => {
+   // res.send('Hello World yall')
+    var spawn = require("child_process").spawn;
+    var process = spawn('python', ["C:/Users/Shoshani/Documents/card-algorithms/Space-Jam/assignment4_5/webpage/compute_input.py", "mayan"]);
 
+    process.stdout.on('data', function (chunk) {
 
-
-process.stdout.on('data', function (chunk) {
-
-    var textChunk = chunk.toString();// buffer to string
-
-    console.log(textChunk);
-});
+        var textChunk = chunk.toString();// buffer to string
+        res.send(textChunk);
+    });
+})
 */
+
+
+
+    
